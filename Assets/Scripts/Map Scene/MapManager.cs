965b2dd7697e4ff94be682fb18cc0c333b1c30b7
rc3-johnson <ryan@ryan-j.co.uk>
2016-10-25 22:48:00 +0100

    Added more to world map, including nukes moving and reversal. Game over is when a nuke hits west korea

---
+++ Assets/Scripts/Map Scene/MapManager.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MapManager : MonoBehaviour {
+
+    public float randomNumer;
+
+    public GameObject[] launchSite;
+    public GameObject westKorea;
+
+    public GameObject nukePrefab;
+
+    float mapTimer;
+
+
+    bool[] nukesLaunched;
+    float[] launchTimes;
+
+    List<GameObject> nukes;
+
+
+    const int NA = 0; //North America
+    const int SA = 1; //South America
+    const int OC = 2; //Oceania
+    const int EU = 3; //Europe
+    const int AS = 4; //Asia
+    const int AF = 5; //Africa
+
+    // Use this for initialization
+    void Start () {
+        mapTimer = 0.0f;
+
+        nukesLaunched = new bool[6];
+        launchTimes = new float[6];
+
+        for(int i = 0; i < 6; i++)
+        {
+            nukesLaunched[i] = false;
+        }
+
+        launchTimes[NA] = 3.0f;
+        launchTimes[SA] = 30.0f;
+        launchTimes[OC] = 60.0f;
+        launchTimes[EU] = 90.0f;
+        launchTimes[AS] = 120.0f;
+        launchTimes[AF] = 150.0f;
+
+        nukes = new List<GameObject>();
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        mapTimer += Time.deltaTime;
+
+        //At 3 seconds launch nuke from North America to West Korea
+        if(mapTimer > 3.0f && nukesLaunched[NA] == false)
+        {
+            nukesLaunched[NA] = true;
+            addNuke(launchSite[NA], westKorea, NA);
+        }
+
+        //At 20 seconds launch nuke from Africa to West Korea
+        if (mapTimer > 20.0f && nukesLaunched[AF] == false)
+        {
+            nukesLaunched[AF] = true;
+            addNuke(launchSite[AF], westKorea, AF);
+        }
+
+        //At 40 seconds launch nuke from South America to West Korea
+        if (mapTimer > 40.0f && nukesLaunched[SA] == false)
+        {
+            nukesLaunched[SA] = true;
+            addNuke(launchSite[SA], westKorea, SA);
+        }
+
+        //At 60 seconds launch nuke from Oceania to West Korea
+        if (mapTimer > 60.0f && nukesLaunched[OC] == false)
+        {
+            nukesLaunched[OC] = true;
+            addNuke(launchSite[OC], westKorea, OC);
+        }
+
+        //At 80 seconds launch nuke from Europe to West Korea
+        if (mapTimer > 80.0f && nukesLaunched[EU] == false)
+        {
+            nukesLaunched[EU] = true;
+            addNuke(launchSite[EU], westKorea, EU);
+        }
+
+        //At 100 seconds launch nuke from Asia to West Korea
+        if (mapTimer > 100.0f && nukesLaunched[AS] == false)
+        {
+            nukesLaunched[AS] = true;
+            addNuke(launchSite[AS], westKorea, AS);
+        }
+
+
+    }
+
+    void addNuke(GameObject origin, GameObject destination, int originID)
+    {
+        GameObject newNuke = (GameObject)Instantiate(Resources.Load("Nuke"));
+        Nuke newNukeControl = newNuke.GetComponent<Nuke>();
+        newNukeControl.setRoute(origin, destination);
+        newNuke.transform.position = origin.transform.position;
+        newNukeControl.setOrigin(originID);
+        nukes.Add(newNuke);
+    }
+
+    public void gameOver()
+    {
+        SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Scripts/Map Scene/Nuke.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Nuke : MonoBehaviour {
+
+
+    GameObject nukeOrigin;
+    GameObject nukeDestination;
+
+    Transform nukeStart;
+    Transform nukeEnd;
+    public float nukeSpeed;
+
+    float startTime;
+    float journeyLength;
+
+    bool nukeEnabled;
+
+    int originID;
+
+    public float fracJourney;
+
+    // Use this for initialization
+    void Start () {
+        fracJourney = 0.0f;
+
+        nukeStart = nukeOrigin.transform;
+        nukeEnd = nukeDestination.transform;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(nukeStart.position, nukeEnd.position);
+
+        nukeEnabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(nukeEnabled)
+        {
+            
+            float distCovered = (Time.time - startTime) * nukeSpeed;
+
+            fracJourney = distCovered / journeyLength;
+
+
+
+            if (fracJourney < 1.0f)
+            {
+
+                Vector3 between = nukeStart.position - nukeEnd.position;
+
+                if(between != Vector3.zero)
+                {
+                    float angle = Mathf.Atan2(between.y, between.x) * Mathf.Rad2Deg;
+                    transform.rotation = Quaternion.AngleAxis(angle + 90.0f, Vector3.forward);
+                }
+
+                
+
+
+                /* Give arc to nuke launch (not working)
+                Vector3 between = nukeStart.position - nukeEnd.position;
+                Vector3 rightAngle = Vector3.Cross(Vector3.up, between);
+                Vector3 normalRight = Vector3.Normalize(rightAngle);
+                Vector3 addSpace = normalRight * (-Mathf.Pow(fracJourney - 0.5f, 2.0f) * 4 + 1);
+                */
+                transform.position = Vector3.Lerp(nukeStart.position, nukeEnd.position, fracJourney);
+
+                // Nuke scales up as it gets to middle of journey, then scales down at the end
+                float nukeSize = (-Mathf.Pow(fracJourney - 0.5f, 2.0f) * 2 + 1) / 6;
+                transform.localScale = new Vector3(nukeSize, nukeSize, nukeSize);
+            }
+            else
+            {
+                //Nuke has reached destination! Explode!
+                if(nukeDestination.name == "WestKorea")
+                {
+                    //Debug.Log("GAME OVER! NUKE LANDED IN WEST KOREA");
+                    GameObject mapManagerObj = GameObject.Find("MapManager");
+                    MapManager mapManager = mapManagerObj.GetComponent<MapManager>();
+                    mapManager.gameOver();
+                }
+                else
+                {
+                    Debug.Log("WELL DONE! NUKE LANDED IN " + nukeDestination.name);
+                    nukeEnabled = false;
+                }
+            }
+        }
+
+       
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("DatingScene", LoadSceneMode.Additive);
+        }
+    }
+
+
+    //Reverse the nuke
+    public void reverseNuke()
+    {
+        
+        //Swap start and end transforms
+        Transform tempTransform = nukeEnd;
+        nukeEnd = nukeStart;
+        nukeStart = tempTransform;
+
+        //Swap start and end game objects
+        GameObject tempGameObj = nukeOrigin;
+        nukeOrigin = nukeDestination;
+        nukeDestination = tempGameObj;
+
+        //Store the old speed for calculates, set new speed to 0.2
+        float oldSpeed = nukeSpeed;
+        nukeSpeed = 0.2f;
+
+        //Calculate the total travel time for nuke to get to current location
+        float travelTime = Time.time - startTime;
+
+        //Calculate the difference in speed
+        float speedDifference = oldSpeed / nukeSpeed;
+
+        //Calculate how long it would have taken to get to the end
+        float estTotalTime = (travelTime / fracJourney);
+
+        //Calculate the inveres percentage of journey (if nuke had gotten 20%, convert to 80%)
+        float inverseFracJourney = (fracJourney * -1) + 1;
+
+        //New total travel time is the old time * inverse fraction * speed differnce
+        float newTravelTime = estTotalTime * inverseFracJourney * speedDifference;
+
+        //New start time is current time - new est travel time
+        startTime = Time.time - newTravelTime;
+
+    }
+
+    public void setRoute(GameObject origin, GameObject destination)
+    {
+        nukeOrigin = origin;
+        nukeDestination = destination;        
+    }
+
+    public void setOrigin(int origin)
+    {
+        originID = origin;
+    }
+
+    public int getOrigin()
+    {
+        return originID;
+    }
+}
