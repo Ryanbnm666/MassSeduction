29777ad89cf883cc3cbda26a4c7237bc4f5159de
n8-webb <niallfcwebb@gmail.com>
2016-10-25 18:01:14 +0100

    Menu added

---
+++ Assets/StartGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+    public Button startGame;
+	// Use this for initialization
+	void Start () {
+        Button btn = startGame.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void TaskOnClick()
+    {
+        Debug.Log("clicked!");
+        SceneManager.LoadScene("WorldScene");
+    }
+}
---
+++ Assets/Travel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Travel : MonoBehaviour {
+    public float speed;
+    public GameObject nuke;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nuke.transform.Translate(Vector2.up * speed * Time.deltaTime);
+	}
+}
